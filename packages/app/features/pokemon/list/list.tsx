ec38d31b983872d00f122ce0bc3d67679fe8b1af
patawayan <pawayan3@gmail.com>
2024-05-15 01:16:12 +0800

    web changes

--- packages/app/features/pokemon/list/list.tsx
+++ packages/app/features/pokemon/list/list.tsx
@@ -1,11 +1,6 @@
 'use client';
 
 import { useContext } from 'react';
-import { Text, YStack } from '@my/ui';
-import { FlatList } from 'react-native';
-import { PokemonContext } from 'app/provider/Pokemon';
-import { PokeListItem } from './list-item';
-import { Spinner } from '@my/ui';
 import { AppContext } from 'app/provider/App';
 import { PokeListNative } from './native';
 import { PokeListWeb } from './web';
